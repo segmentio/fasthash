de7e02e8fd098ee55dbe6e06a20e573db865c375
Achille Roussel <achille@segment.com>
2017-06-12 19:04:01 -0700

    hash uint64 as a big-endian byte sequence

--- fnv1a/hash_test.go
+++ fnv1a/hash_test.go
@@ -1,6 +1,7 @@
 package fnv1a
 
 import (
+	"encoding/binary"
 	"hash/fnv"
 	"testing"
 )
@@ -19,6 +20,20 @@ func TestHash64(t *testing.T) {
 					t.Errorf("invalid hash, expected %d but got %d", sum1, sum2)
 				}
 			})
+		}
+	})
+
+	t.Run("ensures that hashing a 8-byte integration yields the same hash value than the equivalent string", func(t *testing.T) {
+		u := uint64(1234567890)
+		b := [8]byte{}
+
+		binary.BigEndian.PutUint64(b[:], u)
+
+		h1 := AddUint64(Init64, u)
+		h2 := AddString64(Init64, string(b[:]))
+
+		if h1 != h2 {
+			t.Errorf("bad hash value: expected %X but found %X", h1, h2)
 		}
 	})
 }
